9858ffc804c87ab635c1052dbbf1468d47e8f89b
Nathaniel Hatch-Martinez <nateageek@nathaniels-air.lan>
2022-12-04 15:57:49 -0600

    Part two completed

--- puzzle_2/src/main.rs
+++ puzzle_2/src/main.rs
@@ -1,7 +1,7 @@
 use std::{fs::File, io::{BufRead, BufReader}, str::FromStr};
 use scan_fmt::scan_fmt_some;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Clone, PartialEq)]
 enum GameHand {
     Rock,
     Paper,
@@ -15,9 +15,9 @@ impl FromStr for GameHand {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
-            "A" | "X" => Ok(GameHand::Rock),
-            "B" | "Y" => Ok(GameHand::Paper),
-            "C" | "Z" => Ok(GameHand::Scissors),
+            "A" => Ok(GameHand::Rock),
+            "B" => Ok(GameHand::Paper),
+            "C" => Ok(GameHand::Scissors),
             _ => Err(GameHandParseError)
         }
     }
@@ -39,6 +39,21 @@ enum Game {
     Loss
 }
 
+#[derive(Debug)]
+struct GameParseError;
+impl FromStr for Game {
+    type Err = GameParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "X" => Ok(Game::Loss),
+            "Y" => Ok(Game::Draw),
+            "Z" => Ok(Game::Win),
+            _ => Err(GameParseError)
+        }
+    }
+}
+
 impl Game {
     fn play(player_hand: &GameHand, opponent_hand: &GameHand) -> Game {
         match (player_hand, opponent_hand) {
@@ -50,6 +65,25 @@ impl Game {
             _ => Game::Loss
         }
     }
+
+    // Returns the hand that will beat the input hand
+    fn winning_hand(hand: &GameHand) -> GameHand {
+        match hand {
+            GameHand::Rock => GameHand::Paper,
+            GameHand::Paper => GameHand::Scissors,
+            GameHand::Scissors => GameHand::Rock,
+        }
+    }
+
+    // Returns the hand that will lose to the input hand
+    fn losing_hand(hand: &GameHand) -> GameHand {
+        match hand {
+            GameHand::Paper => GameHand::Rock,
+            GameHand::Scissors => GameHand::Paper,
+            GameHand::Rock => GameHand::Scissors,
+        }
+    }
+
     fn game_score(game_result: &Game) -> i32 {
         if game_result == &Game::Win {
             6
@@ -57,6 +91,13 @@ impl Game {
             3
         } else {
             0
+        }
+    }
+    fn player_expected_hand(opponent_hand: &GameHand, game_result: &Game) -> GameHand {
+        match game_result {
+            Game::Draw => opponent_hand.clone(),
+            Game::Win  => Game::winning_hand(opponent_hand),
+            Game::Loss => Game::losing_hand(opponent_hand)
         }
     }
 }
@@ -73,21 +114,20 @@ fn main() {
         // Read input from line
         let line = line.unwrap();
         
-        let (opponent_hand_input, player_hand_input) = scan_fmt_some!(
+        let (opponent_hand_input, game_outcome_input) = scan_fmt_some!(
             line.as_str(),
             "{} {}",
             String, String
         );
 
-        let player_hand = GameHand::from_str(player_hand_input.unwrap().as_str()).unwrap();
+        let game_outcome = Game::from_str(game_outcome_input.unwrap().as_str()).unwrap();
+        let game_outcome_score = Game::game_score(&game_outcome);
         let opponent_hand = GameHand::from_str(opponent_hand_input.unwrap().as_str()).unwrap();
         
-        let game_results = Game::play(&player_hand, &opponent_hand);
-        let win_loss_score = Game::game_score(&game_results);
-
+        let player_hand = Game::player_expected_hand(&opponent_hand, &game_outcome);
         let player_hand_score = GameHand::hand_score(&player_hand);
         
-        total_score += player_hand_score + win_loss_score;
+        total_score += player_hand_score + game_outcome_score;
     }
     println!("File total score: {}", total_score);
 }
@@ -117,7 +157,7 @@ mod tests {
         assert_eq!(scissors, GameHand::Scissors);
     }
 
-    mod example_games {
+    mod example_games_part_one {
         use std::str::FromStr;
         use crate::{GameHand, Game};
 
@@ -129,6 +169,43 @@ mod tests {
             assert_eq!(game_result, Game::Win);
             let game_score = Game::game_score(&game_result);
             assert_eq!(game_score, 6)
+        }
+    }
+    mod example_games_part_two {
+        use std::str::FromStr;
+        use crate::{GameHand, Game};
+
+        #[test]
+        fn example_one() {
+            let opponent_hand = GameHand::from_str("A").unwrap();
+            let game_result = Game::from_str("Y").unwrap();
+            let player_hand = Game::player_expected_hand(&opponent_hand, &game_result);
+
+            let game_score = Game::game_score(&game_result);
+            let player_score = GameHand::hand_score(&player_hand);
+            assert_eq!(game_score + player_score, 4);
+        }
+
+        #[test]
+        fn example_two() {
+            let opponent_hand = GameHand::from_str("B").unwrap();
+            let game_result = Game::from_str("X").unwrap();
+            let player_hand = Game::player_expected_hand(&opponent_hand, &game_result);
+
+            let game_score = Game::game_score(&game_result);
+            let player_score = GameHand::hand_score(&player_hand);
+            assert_eq!(game_score + player_score, 1);
+        }
+        #[test]
+
+        fn example_three() {
+            let opponent_hand = GameHand::from_str("C").unwrap();
+            let game_result = Game::from_str("Z").unwrap();
+            let player_hand = Game::player_expected_hand(&opponent_hand, &game_result);
+
+            let game_score = Game::game_score(&game_result);
+            let player_score = GameHand::hand_score(&player_hand);
+            assert_eq!(game_score + player_score, 7);
         }
     }
 }
