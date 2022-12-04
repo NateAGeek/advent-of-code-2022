f959ee1e346962b2285cd81754d22737452dcecf
Nathaniel Hatch-Martinez <nateageek@nathaniels-air.lan>
2022-12-04 17:55:08 -0600

    Day 3 part 2 done.

--- puzzle_3/src/main.rs
+++ puzzle_3/src/main.rs
@@ -3,12 +3,15 @@ use std::{collections::HashMap, fs::File, io::{BufReader, BufRead}};
 fn main() {
     let input_file = File::open("./input.txt").unwrap();
     let input_file_reader = BufReader::new(input_file);
+    let mut lines = input_file_reader.lines();
 
     let mut total_sum = 0;
-    for line in input_file_reader.lines() {
-        let line = line.unwrap();
-        let (first_compartment, second_compartment) = line.split_at(line.len()/2);
-        let item = find_match_item_in_compartments(first_compartment, second_compartment);
+    while let (Some(first_sack), Some(second_sack), Some(third_sack)) = (lines.next(), lines.next(), lines.next()) {
+        let item = find_match_item_in_compartments(
+            first_sack.unwrap().as_str(),
+            second_sack.unwrap().as_str(),
+            third_sack.unwrap().as_str()
+        );
         let item_value = item_value(item);
 
         total_sum += item_value;
@@ -17,7 +20,7 @@ fn main() {
     println!("Total Sum For Items: {}", total_sum);
 }
 
-fn find_match_item_in_compartments(first_compartment: &str, second_compartment: &str) -> char {
+fn find_match_item_in_compartments(first_compartment: &str, second_compartment: &str, third_compartment: &str) -> char {
     let mut item_count_hash: HashMap<char, i8> = HashMap::new();
 
     for char in first_compartment.chars() {
@@ -28,7 +31,15 @@ fn find_match_item_in_compartments(first_compartment: &str, second_compartment: 
     for char in second_compartment.chars() {
         let char = item_count_hash.get_mut(&char);
         if let Some(entry) = char {
-            if entry < &mut 2 {
+            if entry == &mut 1 {
+                *entry += 1;
+            }
+        }
+    }
+    for char in third_compartment.chars() {
+        let char = item_count_hash.get_mut(&char);
+        if let Some(entry) = char {
+            if entry == &mut 2 {
                 *entry += 1;
             }
         }
@@ -46,7 +57,7 @@ fn item_value(item: char) -> u32 {
     if item.is_ascii_lowercase() {
         item as u32 - 96 // convert ascii a-z (97 - 122) to range (1-26)
     } else if item.is_ascii_uppercase() {
-        (item as u32 - 65) + 27 // convert ascii a-z (65 - 122) to range (1-26)
+        (item as u32 - 65) + 27 // convert ascii a-z (65 - 122) to range (27 - 52)
     } else {
         0 //Not valid? Should probably throw an err
     }
@@ -58,14 +69,13 @@ mod tests {
     mod examples_part_one {
         use crate::{find_match_item_in_compartments, item_value};
 
-        
         #[test]
         fn example_one() {
             let line = "vJrwpWtwJgWrhcsFMMfFFhFp";
             let (first_compartment, second_compartment) = line.split_at(line.len()/2);
             assert_eq!("vJrwpWtwJgWr", first_compartment);
             assert_eq!("hcsFMMfFFhFp", second_compartment);
-            assert_eq!('p', find_match_item_in_compartments(first_compartment, second_compartment));
+            // assert_eq!('p', find_match_item_in_compartments(first_compartment, second_compartment));
             assert_eq!(16, item_value('p'));
         }
         #[test]
@@ -74,7 +84,7 @@ mod tests {
             let (first_compartment, second_compartment) = line.split_at(line.len()/2);
             assert_eq!("jqHRNqRjqzjGDLGL", first_compartment);
             assert_eq!("rsFMfFZSrLrFZsSL", second_compartment);
-            assert_eq!('L', find_match_item_in_compartments(first_compartment, second_compartment));
+            // assert_eq!('L', find_match_item_in_compartments(first_compartment, second_compartment));
             assert_eq!(38, item_value('L'));
         }
         #[test]
@@ -83,8 +93,30 @@ mod tests {
             let (first_compartment, second_compartment) = line.split_at(line.len()/2);
             assert_eq!("PmmdzqPrV", first_compartment);
             assert_eq!("vPwwTWBwg", second_compartment);
-            assert_eq!('P', find_match_item_in_compartments(first_compartment, second_compartment));
+            // assert_eq!('P', find_match_item_in_compartments(first_compartment, second_compartment));
             assert_eq!(42, item_value('P'));
+        }
+    }
+    mod example_part_two {
+        use crate::{find_match_item_in_compartments, item_value};
+
+        #[test]
+        fn example_one() {
+            let first_sack = "vJrwpWtwJgWrhcsFMMfFFhFp";
+            let second_sack = "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL";
+            let third_sack = "PmmdzqPrVvPwwTWBwg";
+            let item = find_match_item_in_compartments(first_sack, second_sack, third_sack);
+            assert_eq!('r', item);
+            assert_eq!(18, item_value(item));
+        }
+        #[test]
+        fn example_two() {
+            let first_sack = "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn";
+            let second_sack = "ttgJtRGJQctTZtZT";
+            let third_sack = "CrZsJsPPZsGzwwsLwLmpwMDw";
+            let item = find_match_item_in_compartments(first_sack, second_sack, third_sack);
+            assert_eq!('Z', item);
+            assert_eq!(52, item_value(item))
         }
     }
 }
