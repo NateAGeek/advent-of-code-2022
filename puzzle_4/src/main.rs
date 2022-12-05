90f59eee99a73e33f5a6c24f2ba4eddaaf2634a6
Nathaniel Hatch-Martinez <nateageek@nathaniels-air.lan>
2022-12-04 20:14:08 -0600

    Puzzle 4 part 2 done.

--- puzzle_4/src/main.rs
+++ puzzle_4/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     for line in input_file_reader.lines() {
         let line = line.unwrap();
         let jobs = get_elves_jobs(&line);
-        if jobs_contains_overlap(&jobs) {
+        if jobs_contains_full_overlap(&jobs) {
             total_sum += 1;
         }
     }
@@ -28,18 +28,14 @@ fn get_elves_jobs(line: &str) -> ((i32, i32),(i32, i32)) {
     (elf_one_jobs, elf_two_jobs)
 }
 
-fn jobs_contains_overlap(jobs: &((i32, i32), (i32, i32))) -> bool {
-    let mut jobs = jobs.clone();
-
+fn jobs_contains_full_overlap(jobs: &((i32, i32), (i32, i32))) -> bool {
     match jobs {
         ((elf_one_start, elf_one_end), (elf_two_start, elf_two_end))
-            // 2-8,3-7
-            // 23-69,23-98
             if 
-                (elf_one_start <= elf_two_start && 
-                elf_one_end >= elf_two_end) ||
-                (elf_one_start >= elf_two_start && 
-                elf_one_end <= elf_two_end)
+                (elf_two_start <= elf_one_end &&
+                elf_two_end >= elf_one_start)
+                // (elf_one_start >= elf_two_start && 
+                // elf_one_end <= elf_two_end)
                 => true,
         _ => false
     }
@@ -48,77 +44,77 @@ fn jobs_contains_overlap(jobs: &((i32, i32), (i32, i32))) -> bool {
 #[cfg(test)]
 mod tests {
     mod part_one {
-        use crate::{get_elves_jobs, jobs_contains_overlap};
+        use crate::{get_elves_jobs, jobs_contains_full_overlap};
 
         #[test]
         fn example_one() {
             let line = "2-4,6-8";
             let jobs = get_elves_jobs(line);
             assert_eq!(jobs, ((2, 4), (6, 8)));
-            assert_eq!(jobs_contains_overlap(&jobs), false);
+            assert_eq!(jobs_contains_full_overlap(&jobs), false);
 
             let line = "6-8,2-4";
             let jobs = get_elves_jobs(line);
             assert_eq!(jobs, ((6, 8), (2, 4)));
-            assert_eq!(jobs_contains_overlap(&jobs), false);
+            assert_eq!(jobs_contains_full_overlap(&jobs), false);
         }
         #[test]
         fn example_two() {
             let line = "2-3,4-5";
             let jobs = get_elves_jobs(line);
-            assert_eq!(jobs_contains_overlap(&jobs), false);
+            assert_eq!(jobs_contains_full_overlap(&jobs), false);
 
             let line = "4-5,2-3";
             let jobs = get_elves_jobs(line);
-            assert_eq!(jobs_contains_overlap(&jobs), false);
+            assert_eq!(jobs_contains_full_overlap(&jobs), false);
         }
         #[test]
         fn example_three() {
             let line = "5-7,7-9";
             let jobs = get_elves_jobs(line);
-            assert_eq!(jobs_contains_overlap(&jobs), false);
+            assert_eq!(jobs_contains_full_overlap(&jobs), false);
 
             let line = "7-9,5-7";
             let jobs = get_elves_jobs(line);
-            assert_eq!(jobs_contains_overlap(&jobs), false);
+            assert_eq!(jobs_contains_full_overlap(&jobs), false);
         }
         #[test]
         fn example_four() {
             let line = "2-8,3-7";
             let jobs = get_elves_jobs(line);
             assert_eq!(jobs, ((2, 8), (3, 7)));
-            assert_eq!(jobs_contains_overlap(&jobs), true);
+            assert_eq!(jobs_contains_full_overlap(&jobs), true);
 
             let line = "3-7,2-8";
             let jobs = get_elves_jobs(line);
             assert_eq!(jobs, ((3, 7), (2, 8)));
-            assert_eq!(jobs_contains_overlap(&jobs), true);
+            assert_eq!(jobs_contains_full_overlap(&jobs), true);
         }
         #[test]
         fn example_five() {
             let line = "6-6,4-6";
             let jobs = get_elves_jobs(line);
-            assert_eq!(jobs_contains_overlap(&jobs), true);
+            assert_eq!(jobs_contains_full_overlap(&jobs), true);
 
             let line = "4-6,6-6";
             let jobs = get_elves_jobs(line);
-            assert_eq!(jobs_contains_overlap(&jobs), true);
+            assert_eq!(jobs_contains_full_overlap(&jobs), true);
         }
         #[test]
         fn example_six() {
             let line = "2-6,4-8";
             let jobs = get_elves_jobs(line);
-            assert_eq!(jobs_contains_overlap(&jobs), false);
+            assert_eq!(jobs_contains_full_overlap(&jobs), false);
 
             let line = "4-8,2-6";
             let jobs = get_elves_jobs(line);
-            assert_eq!(jobs_contains_overlap(&jobs), false);
+            assert_eq!(jobs_contains_full_overlap(&jobs), false);
         }
         #[test]
         fn example_from_file() {
             let line = "23-69,23-98";
             let jobs = get_elves_jobs(line);
-            assert_eq!(jobs_contains_overlap(&jobs), true);
+            assert_eq!(jobs_contains_full_overlap(&jobs), true);
         }
     }
 }
